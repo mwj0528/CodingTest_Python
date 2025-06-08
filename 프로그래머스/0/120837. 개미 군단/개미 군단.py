10111efcb7890fc4c86fa660ed0fa18f82d99fdd
Minwoo Jung <127863800+mwj0528@users.noreply.github.com>
2025-06-08 14:47:46 +0900

    [level 0] Title: 개미 군단, Time: 0.00 ms, Memory: 9.27 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.py
@@ -0,0 +1,15 @@
+def solution(hp):
+    answer = 0
+    while hp != 0:
+        if hp >= 5:
+            answer += (hp // 5)
+            hp %= 5 
+
+        elif hp >= 3:
+            answer += (hp // 3)
+            hp %= 3
+            
+        else:
+            answer += hp
+            hp = 0
+    return answer
