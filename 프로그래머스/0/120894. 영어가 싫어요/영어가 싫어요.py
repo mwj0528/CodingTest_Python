9e475865d02d668c13a1ee054b110ddd4810f4b2
Minwoo Jung <127863800+mwj0528@users.noreply.github.com>
2025-06-09 14:23:32 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.03 ms, Memory: 9.48 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -0,0 +1,13 @@
+def solution(numbers):
+    num_dict = {"zero": "0", "one": "1", "two": "2", "three": "3",
+                "four": "4", "five": "5", "six": "6", "seven": "7",
+                "eight": "8", "nine": "9"}
+    
+    word = ""
+    result = ""
+    for ch in numbers:
+        word += ch
+        if word in num_dict:
+            result += num_dict[word]
+            word = ""
+    return int(result)
