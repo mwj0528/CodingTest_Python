6ec1fea103c598fa33ecbd9c692137e4320e7fd4
Minwoo Jung <127863800+mwj0528@users.noreply.github.com>
2025-06-29 16:36:26 +0900

    [level 1] Title: 모의고사, Time: 3.71 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,34 +1,19 @@
+# 1,2,3,4,5,2,3,4
+# 0,1,2,3,4,5,6,7
 def solution(answers):
     answer = []
-    ans_1 = [1,2,3,4,5]
-    ans_2 = [2,1,2,3,2,4,2,5]
-    ans_3 = [3,3,1,1,2,2,4,4,5,5]
-    if len(answers) > 10:
-        ans_1 *= (len(answers) // 5 + 1)
-        ans_2 *= (len(answers) // 8 + 1)
-        ans_3 *= (len(answers) // 10 + 1)
-        
-    elif len(answers) > 8:
-        ans_1 *= (len(answers) // 5 + 1)
-        ans_2 *= (len(answers) // 8 + 1)
-    elif len(answers) > 5:
-        ans_1 *= (len(answers) // 5 + 1)
-    ans_1 = ans_1[:len(answers)]
-    ans_2 = ans_2[:len(answers)]
-    ans_3 = ans_3[:len(answers)]
-    
-    cnt_1, cnt_2, cnt_3 = 0, 0, 0
-    for i in range(len(answers)):
-        if ans_1[i] == answers[i]:
-            cnt_1 += 1
-        if ans_2[i] == answers[i]:
-            cnt_2 += 1
-        if ans_3[i] == answers[i]:
-            cnt_3 += 1
-    
-    cnt = [cnt_1, cnt_2, cnt_3]
-    max_cnt = max(cnt)
-    for i in range(len(cnt)):
-        if cnt[i] == max_cnt:
+    cnt = [0, 0, 0]
+    p_1 = [1, 2, 3, 4, 5]
+    p_2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    p_3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    for idx, cor in enumerate(answers):
+        if cor == p_1[idx % len(p_1)]:
+            cnt[0] += 1
+        if cor == p_2[idx % len(p_2)]:
+            cnt[1] += 1
+        if cor == p_3[idx % len(p_3)]:
+            cnt[2] += 1
+    for i, v in enumerate(cnt):
+        if v == max(cnt):
             answer.append(i+1)
     return answer
