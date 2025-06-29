9127392f63c2f4c261f4af4f685151d77e80369a
Minwoo Jung <127863800+mwj0528@users.noreply.github.com>
2025-06-29 12:52:37 +0900

    [level 1] Title: 폰켓몬, Time: 1.32 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,11 +1,7 @@
 def solution(nums):
+    print(len(set(nums)))
     
-    n_dict = dict()                   # Hash
-
-    for n in nums:
-        n_dict[n] = 1                 # 같은 종류의 폰켓몬의 중복이 제거된다.
+    if len(set(nums)) >= len(nums)/2:
+        return (len(nums)//2)
+    else: return len(set(nums))
         
-    if len(nums) // 2 <= len(n_dict): # 전체 종류의 수/2와 중복 제거된 종류의 수를 비교
-        return len(nums) // 2         # 전체 종류의 수/2값이 작거나 같다면 즉시 반환
-    
-    return len(n_dict)                # 그렇기 않다면 중복 제거된 종류의 수를 반환
