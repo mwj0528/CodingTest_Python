45b9efd9d4c0632308f9e5c6a00f13f6543ec9d1
Minwoo Jung <127863800+mwj0528@users.noreply.github.com>
2025-06-29 15:41:36 +0900

    [level 2] Title: H-Index, Time: 0.22 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,10 +1,8 @@
-# h번 이사 인용된 논문이 h편 이상 -> h의 최댓값이 h-index
-# [6,5,3,1,0]
-# [0 1 2 3 4] 
 def solution(citations):
-    n = len(citations)
-    citations.sort(reverse=True)
-    for i, c in enumerate(citations):
-        if i + 1 > c:
-            return i
+    citations.sort(reverse = True)
+
+    for idx, citation in enumerate(citations):
+        if  idx + 1 > citation:
+            return idx
+    
     return len(citations)
