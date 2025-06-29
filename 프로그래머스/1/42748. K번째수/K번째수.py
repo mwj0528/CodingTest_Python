0c1fa91d10b5b8e521262c41edb4845260efd894
Minwoo Jung <127863800+mwj0528@users.noreply.github.com>
2025-06-29 15:27:51 +0900

    [level 1] Title: K번째수, Time: 0.02 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,11 +1,8 @@
 def solution(array, commands):
     answer = []
     for command in commands:
-        # print(command)
-        i,j,k = command[0], command[1], command[2]
-        slice_array = array[i-1:j]
-        slice_array.sort()
-        
-        answer.append(slice_array[k-1])
-    
+        i, j, k = command[0], command[1], command[2]
+        arr = sorted(array[i-1:j])
+        print(arr)
+        answer.append(arr[k-1])
     return answer
