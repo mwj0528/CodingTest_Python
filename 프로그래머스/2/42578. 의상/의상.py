0ce72fca80b60965d55320516904eebdd3c8c71b
Minwoo Jung <127863800+mwj0528@users.noreply.github.com>
2025-06-29 13:22:27 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,16 +1,14 @@
 def solution(clothes):
-    # 각 종류별 가진 의상을 저장 (종류:[이름, 이름, ...])
-    closet = {} 
-    for name, kind in clothes:
-        if kind in closet.keys():
-            closet[kind] += [name]
+    dic = {}
+    for cloth in clothes:
+        v, k = cloth[0], cloth[1]
+        if k in dic:
+            dic[k] += [v]
         else:
-            closet[kind] = [name]
-    # {'headgear': ['yellow_hat', 'green_turban'], 'eyewear': ['blue_sunglasses']}
+            dic[k] = [v]
     
-    # A의 종류가 N개, B의 종류가 M개 일 때 가능한 모든 경우의 수 (N+1)(M+1)
     answer = 1
-    for _, value in closet.items():
+    for _, value in dic.items():
         answer *= (len(value) + 1)
-        
-    return answer -1
+    
+    return answer - 1
