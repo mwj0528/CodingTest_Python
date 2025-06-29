b021a518f34db295d755bad83c438af6ec1eb4c3
Minwoo Jung <127863800+mwj0528@users.noreply.github.com>
2025-06-29 22:51:10 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 351.67 ms, Memory: 9.32 MB -BaekjoonHub

---
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -0,0 +1,8 @@
+def solution(n, wires):
+    ans = n
+    for sub in (wires[i+1:] + wires[:i] for i in range(len(wires))):
+        s = set(sub[0])
+        # 집합연산자 & : 교집합 연산,   집합연산자 update : 여러데이터를 한번에 추가
+        [s.update(v) for _ in sub for v in sub if set(v) & s]  
+        ans = min(ans, abs(2 * len(s) - n))
+    return ans
