9f1ea23ae3b4c0a3ae2b097d78c85eafcbc85610
Minwoo Jung <127863800+mwj0528@users.noreply.github.com>
2025-06-30 22:34:38 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.73 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,24 +1,25 @@
 from collections import deque
-
 def solution(maps):
-    
-    n, m = len(maps), len(maps[0])
-    
-    queue = deque()
-    queue.append((0,0))
-    dx = [1,-1,0,0]
-    dy = [0,0,1,-1]
-    while queue:
-        x, y = queue.popleft()          
-            
-        for i in range(4):
-            nx, ny = x +dx[i], y+dy[i]
-                
-            if 0 <= nx < m and 0 <= ny < n:
-                if maps[ny][nx] == 1:
-                    maps[ny][nx] = maps[y][x] + 1
-                    queue.append((nx, ny))
-    if maps[n-1][m-1] == 1:
-            return -1
-    else: 
-        return maps[n-1][m-1]
+    n = len(maps[0])
+    m = len(maps)
+    def bfs():
+        queue = deque()
+        queue.append((0,0))
+        
+        dx = [1, -1, 0, 0]
+        dy = [0, 0, 1, -1]
+        
+        while queue:
+            x,y = queue.popleft()
+            for i in range(4):
+                nx, ny = x + dx[i], y + dy[i]
+                if 0 <= nx < n and 0 <= ny < m:
+                    if maps[ny][nx] == 1:
+                        maps[ny][nx] = maps[y][x] + 1
+                        queue.append((nx, ny))
+        
+        return maps[m-1][n-1]
+    result = bfs()
+    if result == 1:
+        return -1
+    return result
